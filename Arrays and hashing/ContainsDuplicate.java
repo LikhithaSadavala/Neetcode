58e523bda2bc959146af5ddb0723237aee46d202
Likhitha S <52026724+LikhithaSadavala@users.noreply.github.com>
2022-06-21 10:14:20 +0530

    Update ContainsDuplicate.java

--- Arrays and hashing/ContainsDuplicate.java
+++ Arrays and hashing/ContainsDuplicate.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> unique = new HashSet<>();
+        for(final int num:nums){
+            if(!unique.add(num)){
+                return true;
+        }
+       }
+        return false;
+    }
+}
