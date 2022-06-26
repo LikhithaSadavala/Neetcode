2cf426b04b491b86383631d098cd3843df86c940
Likhitha S <52026724+LikhithaSadavala@users.noreply.github.com>
2022-06-26 23:28:42 +0530

    Create ValidSudoku.java

---
+++ Arrays and hashing/ValidSudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> h1 = new HashSet<String>();
+        
+        for(int i=0; i < 9; i++){
+            for(int j=0; j< 9; j++){
+                if(board[i][j] != '.'){
+                    
+                if(h1.contains("row" + i + board[i][j]) || h1.contains("col" + j + board[i][j]) ){
+                    return false;
+                }
+                h1.add("row" + i + board[i][j]);
+                h1.add("col" + j + board[i][j]);
+                
+                if(h1.contains("box"+ (i/3) + (j/3) + board[i][j])){
+                    return false;
+                }
+                h1.add("box"+ (i/3) + (j/3) + board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+}
